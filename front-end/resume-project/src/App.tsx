156d4e66c4be6e4ca529d56b4228a2690e4eee9e
Jinyeong <jinyeongpark1205@gamil.com>
2023-08-08 19:36:45 +0900

    LoginForm component

--- front-end/resume-project/src/App.tsx
+++ front-end/resume-project/src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import LoginForm from "./components/LoginForm/LoginForm";
 
 function App() {
   return (
     <>
       <h1>Resume builder</h1>
-      <NavBar />
+      {/* <NavBar /> */}
+      <LoginForm />
     </>
   );
 }
